edbdcd02f28d0eb84fede85f567061c4f7ce5004
digitalmine <34872176+digitalmine@users.noreply.github.com>
2017-12-31 13:34:20 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -145,7 +145,7 @@ wget https://github.com/polispay/polis/releases/download/v1.1.0/poliscore-1.1.0-
 echo && echo "Installing poliscore-1.1.0..."
 echo
 sleep 3
-unzip -v poliscore-1.1.0-linux.zip
+unzip ~/poliscore-1.1.0-linux.zip
 sudo cp ~/poliscore-1.1.0-linux/usr/local/bin/polis{d,-cli} /usr/bin
 
 # Create config for poliscore
